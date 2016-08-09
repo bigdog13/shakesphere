be5cf04472d106225c5cddc8c01b52597f8b73bf
bigdog13 <you@example.com>
2016-08-09 08:10:59 -0400

    second commit

---
+++ read all lines/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace read_all_lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //object
+            string line;
+            StreamReader reader = new StreamReader("TextFile1.txt");
+            using (reader)
+
+                int lineNumer = 0;
+            //read first line
+            stringline = reader.ReadLine();
+            {
+                {
+                    line = reader.ReadLine();
+
+
+                }
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ shakesphere/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace shakesphere
+{
+    class Program
+    {//read first line
+        static void Main(string[] args)
+        { //object
+          /* string line;
+           StreamReader reader = new StreamReader("TextFile1.txt");
+           using (reader)
+
+               //read the first line from the text file
+           {
+               {
+                   line = reader.ReadLine();
+
+
+               }
+               Console.WriteLine(line);
+               */
+
+
+
+            //write all of text
+            /*
+            StreamReader reader = new StreamReader("TextFile1.txt");
+            using (reader)
+            {
+                int lineNumber = 0;
+                //read first line
+                string line = reader.ReadLine();
+                //read the other lines for the text file
+                while(line != null)
+                {
+                    lineNumber++;
+                    Console.WriteLine("Line{0}: {1}", lineNumber, line);
+                    line = reader.ReadLine();
+                }*/
+
+
+            /*
+            ///write file capitl all lowercase vowels
+            StreamReader reader = new StreamReader("TextFile1.txt");
+            string line;
+            using (reader)
+            {//object
+
+                line = reader.ReadLine();
+
+                while (line != null) ;
+
+
+                switch (i)
+                {//return capital vowel
+                    case 'a': return 'A';
+                    case 'e': return 'E';
+                    case 'i': return 'I';
+                    case 'o': return 'O';
+                    case 'u': return 'U';
+
+                    default:
+                        return (char)(i);
+
+
+
+                }
+                  Console.WriteLine();
+                  line = reader.ReadLine();  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+
+}              
+
